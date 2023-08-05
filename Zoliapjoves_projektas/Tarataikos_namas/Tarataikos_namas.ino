cb47176cbc5755521600bc8125c522ffe621e757
Justinas Valius <129941913+justinas244@users.noreply.github.com>
2023-08-05 23:41:03 +0300

    Minimalus pataisymai
    Prideta sviesos busena kai griztama namo
    Perkeista pradine busena ijungus krovimo stoti, kad iskart po vartu atidarymo isijungtu krovimas

--- Zoliapjoves_projektas/Tarataikos_namas/Tarataikos_namas.ino
+++ Zoliapjoves_projektas/Tarataikos_namas/Tarataikos_namas.ino
@@ -40,13 +40,17 @@ byte duru_state = 2;
 int atidarymo_mygtukas, uzdarymo_mygtukas, centro_mygtukas, stogo_mygtukas;
 int siusta = 0;
 int aptiktas_robotas = 0;
-int busena = 4;
+int busena = 1;
 int laido_modas = 0;
 int d[4] = {0, 0, 0, 0};
+
+int lietaus_pin = 6;
+int lietus = 0;
 void setup() {
   sr.setAllLow(); // set all pins LOW
   pinMode(atidarymo_pin, INPUT_PULLUP);
   pinMode(uzdarymo_pin, INPUT_PULLUP);
+  pinMode(lietaus_pin, INPUT);
   Serial.begin(9600);
   robotas.begin(9600);
   dht.begin();
@@ -79,35 +83,41 @@ void loop() {
     sr.set(Perimetras, LOW);
     sr.set(Namu_perimetras, LOW);
     motor(0);
+    Vidaus_led(0);
   }
   else if(busena==1) {
     sr.set(Krovimo, HIGH);
     sr.set(Perimetras, LOW);
     sr.set(Namu_perimetras, LOW);
+    Vidaus_led(0);
   }
   else if(busena==2) {
     sr.set(Perimetras, HIGH);
     sr.set(Namu_perimetras, LOW);
     sr.set(Krovimo, LOW);
+    Vidaus_led(0);
   }
   else if(busena==3) {
     sr.set(Namu_perimetras, HIGH);
     sr.set(Perimetras, LOW);
     sr.set(Krovimo, LOW);
+    Vidaus_led(1);
   }
   else if(busena==4) {
     sr.set(Perimetras, HIGH);
     sr.set(Namu_perimetras, HIGH);
     sr.set(Krovimo, LOW);
+    Vidaus_led(1);
   }
   else if(busena==5) {
     sr.set(Perimetras, LOW);
     sr.set(Namu_perimetras, HIGH);
     sr.set(Krovimo, HIGH);
+    Vidaus_led(1);
   }
   else if(busena==6) {
     mygtuku_nuskaitymas();
-    if(atidarymo_mygtukas == LOW){ busena = 0; }
+    if(atidarymo_mygtukas == LOW){ busena = 10; }
     else{ atidarymas_vartu(); }
     duru_state = 3;
   }
@@ -125,7 +135,8 @@ void mygtuku_nuskaitymas() {
   uzdarymo_mygtukas = digitalRead(uzdarymo_pin);
   centro_mygtukas =  analogRead(centro_pin);
   stogo_mygtukas = analogRead(stogo_pin);
-
+  lietus = digitalRead(lietaus_pin);
+  
   d[0] =  duru_state;
   if (centro_mygtukas >= 512) {
     d[1] = 0;
@@ -133,7 +144,9 @@ void mygtuku_nuskaitymas() {
   else {
     d[1] = 1;
   }
-  d[2] = 0;
+  if(lietus == HIGH) { d[2] = 2; }
+  else{ d[2] = 1; }
+  
   d[3] = 0;
 }
 void motor(int state) {
